1ad6e4672b06472520b9b4eaf6c14bfbf5390133
parikshit-pb <papon46857@gmail.com>
2026-03-30 23:35:57 +0600

    added navbar $ banner section

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './component/Navbar/Navbar';
+import Hero from './component/Hero/Hero';
+
+const App = () => {
+  return (
+   
+   <div>
+    <Navbar />
+    <Hero></Hero>
+   </div>
+   
+  );
+};
+
+export default App;
---
+++ src/component/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import HeroPng from "../../assets/banner.png"
+
+const Hero = () => {
+    return (
+      <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <img src={HeroPng} alt="" />
+    <div>
+      <h1 className="text-5xl font-bold">Supercharge Your <br /> Digital Workflow
+</h1>
+      <p className="py-6">
+        Access premium AI tools, design assets, templates, and productivity <br />
+        software—all in one place. Start creating faster today. <br /> Explore Products
+      </p>
+      <button className="btn btn-primary">Explore Products</button>
+      <button className="btn btn-primary">Watch Demo</button>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Hero;
